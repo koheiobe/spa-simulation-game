2fd7e504f44e9aacd099e3dc7fa32c19fb1db09d
Kohei Obe <gaku1025s080565@yahoo.co.jp>
2020-05-03 12:44:58 +0900

    modify: vue bootstrapのエラーが消えていたので、不要なtype宣言を削除

--- types/shims-vue.d.ts
+++ types/shims-vue.d.ts
@@ -3,10 +3,3 @@ import Vue from 'vue'
 declare module '*.vue' {
   export default Vue
 }
-
-// nuxtのconsole上でbvToastのエラーが出てしまうため定義。
-declare module 'vue/types/vue' {
-  interface Vue {
-    $bvToast: any
-  }
-}
